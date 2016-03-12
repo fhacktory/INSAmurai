feb2de95135642c61ce144a316750b03dd3b282d
Maxence Souchon <maxence.souchon@insa-lyon.fr>
2016-03-12 15:37:14 +0100

    git storm ended \o/ take a better quality photo

--- app/src/main/java/com/fhactory/sketchracer/PhotoActivity.java
+++ app/src/main/java/com/fhactory/sketchracer/PhotoActivity.java
@@ -1,17 +1,26 @@
 package com.fhactory.sketchracer;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class PhotoActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private File photoFile;
 
     private static final int CAMERA_REQUEST_CODE = 12345;
 
@@ -28,11 +37,19 @@ public class PhotoActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        cleanTempDir(this);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == CAMERA_REQUEST_CODE) {
             if(resultCode == RESULT_OK) {
-                Bitmap photo = (Bitmap) data.getExtras().get("data");
-                imageView.setImageBitmap(photo);
+                Bitmap photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                System.out.println(photoFile.getAbsolutePath());
+                imageView.setImageBitmap(getResizedBitmap(photo));
             } else if(resultCode == RESULT_CANCELED) {
                 new AlertDialog.Builder(this)
                     .setMessage(getString(R.string.must_take_photo))
@@ -58,6 +75,62 @@ public class PhotoActivity extends AppCompatActivity {
     private void takePhoto() {
         //demander à prendre une photo
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            try {
+                photoFile = File.createTempFile(
+                        "photo",  /* prefix */
+                        ".jpg",         /* suffix */
+                        getExternalFilesDir("temporary")      /* directory */
+                );
+
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+            } catch (IOException e) {
+                new AlertDialog.Builder(this)
+                    .setMessage(getString(R.string.photo_error))
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    });
+            }
+        } else {
+            new AlertDialog.Builder(this)
+                .setMessage(getString(R.string.photo_error))
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        }
+    }
+
+    public static Bitmap getResizedBitmap(Bitmap bitmap) {
+        int maxHeight = 2048;
+        int maxWidth = 2048;
+        float scale = Math.min(((float)maxHeight / bitmap.getWidth()), ((float)maxWidth / bitmap.getHeight()));
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);
+
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    public static void cleanTempDir(Context c) {
+        File directory = c.getExternalFilesDir("temporary");
+        if (directory != null) {
+            File[] list = directory.listFiles();
+            for(File f : list) {
+                if(f.delete()) {
+                    Log.d("CleanTempDir", f.getName()+" deleted");
+                } else {
+                    Log.e("CleanTempDir", f.getName()+" not deleted!");
+                }
+            }
+        }
     }
 }
